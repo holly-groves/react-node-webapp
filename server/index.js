9aacb9948e232d6448e769859fee3fd589c5e0d9
Holly Groves <43141062+holly-groves@users.noreply.github.com>
2024-06-07 00:00:49 +0100

    added post endpoint
    added post endpoint to receive messages with simple validation

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-// server/index.js
+const express = require('express'); 
+const bodyParser = require('body-parser');
 
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // local host port
 
 const app = express();
 
+app.use(bodyParser.json()); // body parser so the request json can be read
+
+const msgs = []; // stores the messages while application is running
+
+// GET endpoint to get a test string
 app.get("/api", (req, res) => {
     res.json({ message: "hello from within the code!"});
 });
+
+// POST endpoint that recieves new messages
+app.post("/message",  (req, res) => {
+    const messageText = req.body.message
+
+    // Validate the request JSON
+    if (!messageText) {
+        res.send("Error: Incorrect JSON format -> Check message exists and is not empty")
+    } else {
+        msgs.push(messageText); // Adds the new message to the msgs array
+
+        // Sends response with the message
+        console.log(messageText);
+        res.json({"Message Recieved" : messageText});
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
