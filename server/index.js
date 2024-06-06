0f2525796817f36c6add314b24d60a8472d1b5aa
Holly Groves <43141062+holly-groves@users.noreply.github.com>
2024-06-06 19:05:44 +0100

    added app.get message
    added app.get message

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+    res.json({ message: "hello from within the code!"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
